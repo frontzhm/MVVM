369491c011ca312d1957345b273810ec0ba58f7c
zhm <frontzhm@163.com>
2018-11-20 16:36:11 +0800

    增加代理

--- src/js/vue.js
+++ src/js/vue.js
@@ -5,12 +5,56 @@ class Vue {
     this.$methods = opt.methods
     // 先数据劫持下
     new Observer(this.$data)
+    // 代理
+    this.proxy(this.$data)
+    this.proxy(this.$methods)
     // compile的作用是把html中指令还有{{}}变成data中的值
     new Compile(this,this.$el)
+
 
   }
   getNode(el) {
     // el可能是字符串可能是node，统一变成node
     return typeof el === 'object' ? el : document.querySelector(el)
   }
+  proxy(obj){
+    Object.keys(obj).forEach(key => {
+      Object.defineProperty(this,key,{
+        configurable:true,
+        enumerable:true,
+        getter(){
+          return obj[key]
+        },
+        set(newValue){
+          let oldValue = obj[key]
+          if(oldValue===newValue){
+            return
+          }
+          obj[key] = newValue
+        }
+      })
+    })
+  }
 }
+
+// proxy 可以单独写出来
+/**
+ * function proxy(beProxyedObj,proxyObj){
+ *   Object.keys(beProxyedObj).forEach(key => {
+      Object.defineProperty(proxyObj,key,{
+        configurable:true,
+        enumerable:true,
+        getter(){
+          return beProxyedObj[key]
+        },
+        set(newValue){
+          let oldValue = beProxyedObj[key]
+          if(oldValue===newValue){
+            return
+          }
+          beProxyedObj[key] = newValue
+        }
+      })
+    })
+ * }
+ */
